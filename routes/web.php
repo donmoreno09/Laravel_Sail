f1438bddc85844ac29d750cf7c6b3a2abf7c81de
Don Moreno <d.moreno@esis-italia.com>
2025-06-25 11:08:24 +0200

    Grouping and Organizing the Routes pt. 1

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/transactions', [TransactionController::class, 'index']);
-Route::get('/transactions/create', [TransactionController::class, 'create']);
-Route::get('/transactions/{transactionId}', [TransactionController::class, 'show']);
-Route::post('/transactions', [TransactionController::class, 'store']);
+Route::prefix(('transactions'))->group(function () {
 
-Route::get('transactions/{transactionId}/process', ProcessTransactionController::class);
+    Route::controller(TransactionController::class)->group(function () {
+        Route::get('/', 'index')->name('transactions.index');
+        Route::get('/create', 'create')->name('transactions.create');
+        Route::get('/{transactionId}', 'show')->name('transactions.show');
+        Route::post('/', 'store')->name('transactions.store');
+    });
+
+    // Route::get('/', [TransactionController::class, 'index']);
+    // Route::get('/create', [TransactionController::class, 'create']);
+    // Route::get('/{transactionId}', [TransactionController::class, 'show']);
+    // Route::post('/', [TransactionController::class, 'store']);
+
+    Route::get('/{transactionId}/process', ProcessTransactionController::class);
+});
+
+// Route::get('/transactions', [TransactionController::class, 'index']);
+// Route::get('/transactions/create', [TransactionController::class, 'create']);
+// Route::get('/transactions/{transactionId}', [TransactionController::class, 'show']);
+// Route::post('/transactions', [TransactionController::class, 'store']);
+
+// Route::get('transactions/{transactionId}/process', ProcessTransactionController::class);
