07a22eafd88da1fe23efe2ac85e7dca6b7501449
Don Moreno <d.moreno@esis-italia.com>
2025-06-25 10:18:42 +0200

    FileType.php, Pattern Globali in AppServiceProvider, did some routes in web.php to understand some concepts

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,42 @@
 <?php
 
+use App\Enums\FileType;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function(){
-    return [
-        'message' => 'Welcome to the dashboard',
-        'status' => 'success'
-    ];
+Route::get('/transactions/{transactionId}', function (int $id) {
+    return 'Showing payment details for transaction id: ' . $id;
+})->where('transactionId', '[0-9]+');
+
+// Route::get('/transactions/{transactionId}/files/{fileId}', function (int $transactionId, int $fileId) {
+//     return 'Showing file with id ' . $fileId . ' for transaction id: ' . $transactionId;
+// });
+
+// Route::get('/transactions/{transactionId}/files/{fileType}', function (int $transactionId, string $fileType) {
+//     return 'Showing file with id ' . $fileType . ' for transaction id: ' . $transactionId;
+// })->whereIn('fileType', ['pdf', 'csv', 'xml']); //Solo se non definisci Enum FileType
+
+Route::get('/transactions/{transactionId}/files/{fileType}', function (int $transactionId, FileType $fileType) {
+    return 'Showing file with id ' . $fileType->value . ' for transaction id: ' . $transactionId;
 });
 
-Route::get('/profile', function () {
-    return [
-        'name' => 'John Doe',
-        'email' => 'johndoe@test.com'
-    ];
+// Route::get('/transactions/{transactionId}/files/{fileId}', function (int $transactionId, int $fileId) {
+//     return 'Showing file with id ' . $fileId . ' for transaction id: ' . $transactionId;
+// })->whereNumber(['transactionId', 'fileId']); // Solo se non vuoi definire i pattern globali
+
+// Route::post('/transactions/{transactionId}/files/{fileId}', function (int $transactionId, int $fileId) {
+//     return 'Showing file with id ' . $fileId . ' for transaction id: ' . $transactionId;
+// });
+
+Route::get('/report/{reportId}', function (Request $request, int $reportId) {
+    $year = $request->get('year');
+    $month = $request->get('month');
+
+    return 'Generating report ' . $reportId . ' for ' . $year . ' and ' . $month;
 });
+
+// /report/975?year=2024&month=5
