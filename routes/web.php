1f67f461a3eb9ba36ada0ea37433f9869c1ca24d
Don Moreno <d.moreno@esis-italia.com>
2025-06-25 11:12:37 +0200

    pt. 3 of organizing the routes

--- routes/web.php
+++ routes/web.php
@@ -8,29 +8,41 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix(('transactions'))->group(function () {
-
+Route::name('transactions.')->prefix('transactions')->group(function () {
     Route::controller(TransactionController::class)->group(function () {
-
-        Route::name('transactions.')->group(function () {
-            Route::get('/', 'index')->name('index');
-            Route::get('/create', 'create')->name('create');
-            Route::get('/{transactionId}', 'show')->name('show');
-            Route::post('/', 'store')->name('store');
-        });
-        // Route::get('/', 'index')->name('transactions.index');
-        // Route::get('/create', 'create')->name('transactions.create');
-        // Route::get('/{transactionId}', 'show')->name('transactions.show');
-        // Route::post('/', 'store')->name('transactions.store');
+        Route::get('/', 'index')->name('index');
+        Route::get('/create', 'create')->name('create');
+        Route::get('/{transactionId}', 'show')->name('show');
+        Route::post('/', 'store')->name('store');
+        Route::get('/{transactionId}/dopcuments')->name('documents');
     });
 
-    // Route::get('/', [TransactionController::class, 'index']);
-    // Route::get('/create', [TransactionController::class, 'create']);
-    // Route::get('/{transactionId}', [TransactionController::class, 'show']);
-    // Route::post('/', [TransactionController::class, 'store']);
-
-    Route::get('/{transactionId}/process', ProcessTransactionController::class);
+    Route::get('/{transactionId}/process', ProcessTransactionController::class)->name('process');
 });
+
+// Route::prefix(('transactions'))->group(function () {
+
+//     Route::controller(TransactionController::class)->group(function () {
+
+//         Route::name('transactions.')->group(function () {
+//             Route::get('/', 'index')->name('index');
+//             Route::get('/create', 'create')->name('create');
+//             Route::get('/{transactionId}', 'show')->name('show');
+//             Route::post('/', 'store')->name('store');
+//         });
+//         // Route::get('/', 'index')->name('transactions.index');
+//         // Route::get('/create', 'create')->name('transactions.create');
+//         // Route::get('/{transactionId}', 'show')->name('transactions.show');
+//         // Route::post('/', 'store')->name('transactions.store');
+//     });
+
+//     // Route::get('/', [TransactionController::class, 'index']);
+//     // Route::get('/create', [TransactionController::class, 'create']);
+//     // Route::get('/{transactionId}', [TransactionController::class, 'show']);
+//     // Route::post('/', [TransactionController::class, 'store']);
+
+//     Route::get('/{transactionId}/process', ProcessTransactionController::class);
+// });
 
 // Route::get('/transactions', [TransactionController::class, 'index']);
 // Route::get('/transactions/create', [TransactionController::class, 'create']);
