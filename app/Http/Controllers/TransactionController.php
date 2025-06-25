cf2da44093bc89d7b1d84adc4572f2aeee660e16
Don Moreno <d.moreno@esis-italia.com>
2025-06-25 10:46:03 +0200

    Clean up the routes, deleted Controller.php because it's not needed, Created an invokable controller with the controller command --invokable, created controllers: Process and Transaction

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TransactionController
+{
+    
+    public function index(): string
+    {
+        return 'Transactions Page';
+    }
+
+    public function show(int $transactionId): string
+    {
+        return 'Showing transaction with id: ' . $transactionId;
+    }
+
+    public function create(): string
+    {
+        return 'Create Transaction Page';
+    }
+
+    public function store(Request $request): string
+    {
+        // Here you would typically handle the request to create a new transaction
+        // For simplicity, we will just return a success message
+        return 'Transaction created successfully';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,16 @@
 <?php
 
-use App\Enums\FileType;
-use Illuminate\Http\Request;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\ProcessTransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/transactions/{transactionId}', function (int $id) {
-    return 'Showing payment details for transaction id: ' . $id;
-})->where('transactionId', '[0-9]+');
+Route::get('/transactions', [TransactionController::class, 'index']);
+Route::get('/transactions/create', [TransactionController::class, 'create']);
+Route::get('/transactions/{transactionId}', [TransactionController::class, 'show']);
+Route::post('/transactions', [TransactionController::class, 'store']);
 
-// Route::get('/transactions/{transactionId}/files/{fileId}', function (int $transactionId, int $fileId) {
-//     return 'Showing file with id ' . $fileId . ' for transaction id: ' . $transactionId;
-// });
-
-// Route::get('/transactions/{transactionId}/files/{fileType}', function (int $transactionId, string $fileType) {
-//     return 'Showing file with id ' . $fileType . ' for transaction id: ' . $transactionId;
-// })->whereIn('fileType', ['pdf', 'csv', 'xml']); //Solo se non definisci Enum FileType
-
-Route::get('/transactions/{transactionId}/files/{fileType}', function (int $transactionId, FileType $fileType) {
-    return 'Showing file with id ' . $fileType->value . ' for transaction id: ' . $transactionId;
-});
-
-// Route::get('/transactions/{transactionId}/files/{fileId}', function (int $transactionId, int $fileId) {
-//     return 'Showing file with id ' . $fileId . ' for transaction id: ' . $transactionId;
-// })->whereNumber(['transactionId', 'fileId']); // Solo se non vuoi definire i pattern globali
-
-// Route::post('/transactions/{transactionId}/files/{fileId}', function (int $transactionId, int $fileId) {
-//     return 'Showing file with id ' . $fileId . ' for transaction id: ' . $transactionId;
-// });
-
-Route::get('/report/{reportId}', function (Request $request, int $reportId) {
-    $year = $request->get('year');
-    $month = $request->get('month');
-
-    return 'Generating report ' . $reportId . ' for ' . $year . ' and ' . $month;
-});
-
-// /report/975?year=2024&month=5
+Route::get('transactions/{transactionId}/process', ProcessTransactionController::class);
