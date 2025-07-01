77e4ed788b64b74e2cc684c2f862a58ca49536e8
Don Moreno <d.moreno@esis-italia.com>
2025-07-01 11:34:20 +0200

    or you can use an alias pt.2

--- app/Facades/Payment.php
+++ app/Facades/Payment.php
@@ -16,6 +16,6 @@ class Payment extends Facade
      */
     protected static function getFacadeAccessor(): string
     {
-        return 'paymentProcessor';
+        return PaymentProcessor::class;
     }
 }
