5b5d0bb1a8607a37a4cc3bd319183f9f14a2755b
Don Moreno <d.moreno@esis-italia.com>
2025-07-04 14:22:45 +0200

    using vite with assets

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -2,9 +2,11 @@
     <nav class="bg-gray-800 text-white p-4 flex items-center">
         <!-- Logo -->
         <div class="mr-8">
-            <svg class="w-8 h-8 text-indigo-400" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
-            </svg>
+            <img 
+                class="size-8"
+                src="{{Vite::asset('resources/images/logo.png') }}" 
+                alt="Your company"
+            >
         </div>
         
         <!-- Nav tabs -->
